4e7a8e31f243045fcc4ebdc3c557852aadf03c68
kaanyalova <76952012+kaanyalova@users.noreply.github.com>
2023-12-24 01:42:42 +0300

    start working on the cli

--- crates/peers_cli/src/main.rs
+++ crates/peers_cli/src/main.rs
@@ -1,3 +1,44 @@
+use clap::Command;
+use clap::{builder::Str, Args, CommandFactory, Parser, Subcommand};
+use peers_core;
+
 fn main() {
-    println!("Hello, world!");
+    #[derive(Parser, Debug)]
+    struct Cli {
+        #[command(subcommand)]
+        command: Option<Commands>,
+    }
+
+    #[derive(Subcommand, Debug)]
+    enum Commands {
+        Add(AddArgs),
+    }
+
+    #[derive(Args, Debug)]
+    struct AddArgs {
+        path: String,
+    }
+
+    let cli = Cli::parse();
+
+    let mut c = Cli::command();
+    match &cli.command {
+        Some(Commands::Add(t)) => add_torrent(&t.path),
+        // if
+        None => no_subcommands(&mut c),
+    };
+}
+
+fn add_torrent(path: &str) {
+    println!("added torrent with path {}", path)
+}
+
+#[cfg(feature = "gui")]
+fn no_subcommands(c: Command) {
+    println!("run gui");
+}
+
+#[cfg(not(feature = "gui"))]
+fn no_subcommands(c: &mut Command) {
+    c.print_help().unwrap();
 }
